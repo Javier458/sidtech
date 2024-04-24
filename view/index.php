5b209b1dd9c2dd91a4c430c2d438754465e91d30
Javier458 <e.javiergalvis@gmail.com>
2024-04-24 15:46:07 -0500

    initial template

---
+++ view/index.php
@@ -0,0 +1,129 @@
+<?php 
+$webTitle = "Inicio | Sid&Tech";
+include_once 'templates/header-html.inc.php';
+?>
+
+<!-- Aquí se agregan los estilos personalizados -->
+<link rel="stylesheet" href="css/index.css">
+
+</head>
+  <body>
+<?php include_once 'templates/nologin-navbar.inc.php'; ?>
+
+<!-- ======= Hero ======= -->
+<section class="container-fluid">
+    <div class="row align-items-center contain-hero px-5">
+      <div class="col-12 col-lg-6 px-1 order-2 order-lg-1">
+        <h1 class="px-5 hero-text">Sistema de Gesti&oacute;n de Servicio T&eacute;cnico</h1> 
+        <h3 class="px-5 pt-4 hero-text">Obt&eacute;n acceso al sistema con las mejores funcionalidades para tu negocio.</h3>
+      </div>
+      <div class="col-12 col-lg-6 hero-img-container order-1 order-lg-2">
+        <img src="img/sys-img.png" class="hero-img" alt="Sistema de Gesti&oacute;n de Servicio T&eacute;cnico"> 
+      </div>    
+    </div>  
+  </section> 
+
+<!-- ======= Aliade Brands ======= -->
+<section class="container-fluid shadow-lg p-3 mb-5 bg-body-tertiary">
+    <div class="row">
+        <div class="col-6 col-md-4 col-lg-2"><img src="img/brand-1.png" class="d-block w-100" alt="..."></div>
+        <div class="col-6 col-md-4 col-lg-2"><img src="img/brand-2.png" class="d-block w-100" alt="..."></div>
+        <div class="col-6 col-md-4 col-lg-2"><img src="img/brand-1.png" class="d-block w-100" alt="..."></div>
+        <div class="col-6 col-md-4 col-lg-2"><img src="img/brand-2.png" class="d-block w-100" alt="..."></div>
+        <div class="col-6 col-md-4 col-lg-2"><img src="img/brand-1.png" class="d-block w-100" alt="..."></div>
+        <div class="col-6 col-md-4 col-lg-2"><img src="img/brand-2.png" class="d-block w-100" alt="..."></div>
+    </div>
+</section>
+
+<!-- ======= About Us ======= -->
+<section class="container-fluid py-5 mb-5">
+    <h1 class="text-center">SOBRE NOSOTROS</h1>
+    <div class="row cont-about-us my-5 mx-5 fs-6">
+        <div class="col-12 col-lg-6 px-5">
+            <p>Hemos desarrollado un sitio web especialmente para tu negocio, que cubre todos los &aacute;mbitos, tanto contables como administrativos, permitiendo que lleves un registro seguro y flexible de todo tu negocio, generando as&iacute; una perdurable confianza en todos tus clientes.</p>
+            <ul class="list-about pt-3">
+                <li class="item-about"><i class="bi bi-check2-all me-2"></i>Facilidad de uso y navegación intuitiva.</li>
+                <li class="item-about"><i class="bi bi-check2-all me-2"></i>Optimizaci&oacute;n para dispositivos m&oacute;viles.</li>
+                <li class="item-about"><i class="bi bi-check2-all me-2"></i>Seguridad y Protecci&oacute;n de Datos.</li>
+            </ul>
+        </div>
+        <div class="col-12 col-lg-6 px-5">
+            <p>Cada d&iacute;a estamos mejorando para ti y tu negocio, implementando nuevas actualizaciones con funciones que estar&aacute;n a la vanguardia y permitan brindar un servicio amigable y oportuno a todos tus clientes, optimizaremos cada función para que puedas personalizarla a tu gusto.
+            </p>
+            <button type="button" class="btn btn-dark mt-4">Leer M&aacute;s...</button>
+        </div>
+    </div>
+</section>
+
+<!-- ======= FAQ ======= -->
+<section class="container-fluid shadow-lg py-5 mb-5">
+    <div class="row">
+        <div class="col-12 col-lg-6">
+        <h1 class="text-center pb-5">PREGUNTAS FRECUENTES</h1>
+            <div class="accordion accordion-flush" id="accordionFlushExample">
+                <div class="accordion-item mx-5">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                        <i class="bi bi-question-circle-fill me-3"></i>¿Puedo generar una orden de servicio?
+                        </button>
+                    </h2>
+                <div id="flush-collapseOne" class="accordion-collapse show" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">¡Claro que s&iacute;! Nuestro sistema est&aacute; enfocado principalmente en esta funcionalidad, adem&aacute;s, no solo podr&aacute;s generar la orden sino que tambi&eacute;n podr&aacute;s enviarla mediante un mensaje de Whatsapp para que tus clientes est&eacute;n al tanto del servicio que se le encuentra realizando a sus dispositivos.</div>
+                    </div>
+                </div>
+                <div class="accordion-item mx-5">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                        <i class="bi bi-question-circle-fill me-3"></i>¿C&oacute;mo uso el sitio web?
+                        </button>
+                    </h2>
+                <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">No te preocupes, contamos con diferentes videos en YouTube que te ayudarán con el manejo del sitio web, notar&aacute;s que es tan sencillo que en unos minutos ser&aacute;s un experto al usarlo.</div>
+                    </div>
+                </div>
+                <div class="accordion-item mx-5">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                        <i class="bi bi-question-circle-fill me-3"></i>¿En que horario est&aacute; disponible el sitio web?
+                        </button>
+                    </h2>
+                <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">El sitio web est&aacute; optimizado para que lo uses las 24 horas del d&iacute;a los 7 dias de la semana, as&iacute; que no ser&aacute; un problema el horario en que desees usarlo.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-6 hero-img-container">
+            <img src="img/sys-img.png" class="hero-img" alt="Sistema de Gesti&oacute;n de Servicio T&eacute;cnico">
+        </div>
+    </div>
+</section>
+
+<!-- ======= Services ======= -->
+<section class="container-fluid">
+    <div class="row services-row mb-5">
+        <h1 class="text-center mb-4">SERVICIOS</h1>
+        <div class="col-6 col-lg-3 service-card">
+            <h1><i class="bi bi-file-earmark-text-fill"></i></h1>
+            <h4>Registro de Ordenes</h4>
+            <p class="pt-2">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+        </div>
+        <div class="col-6 col-lg-3 service-card">
+        <h1><i class="bi bi-buildings-fill"></i></h1>
+            <h4>Control de tu Negocio</h4>
+            <p class="pt-2">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+        </div>
+        <div class="col-6 col-lg-3 service-card">
+            <h1><i class="bi bi-cash-coin"></i></h1>
+            <h4>Gesti&oacute;n de Ventas</h4>
+            <p class="pt-2">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+        </div>
+        <div class="col-6 col-lg-3 service-card">
+            <h1><i class="bi bi-gear-fill"></i></h1>
+            <h4>Gesti&oacute;n de Servicios</h4>
+            <p class="pt-2">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+        </div>
+    </div>
+</section>
+
+<?php include_once 'templates/end-html.inc.php'; ?>
---
+++ view/templates/header-html.inc.php
@@ -0,0 +1,26 @@
+<?php
+// Válida si el titulo de la página está vacío
+if(empty($webTitle))
+        $webTitle = "Sid&Tech";
+
+?>
+
+<!Doctype html>
+<html lang="es">
+  <head>
+    <!-- ======= Metadatos ======= -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Javier Galvis">
+    <meta name="description" content="Sistema Web Gestor de Servicio Tecnico">
+    <meta name="keywords" content="servicio, tecnico, celulares, administracion, contabilidad, ordenes, entrada, salida">
+    
+    <!-- ======= Titulo de la pagina ======= -->
+    <title><?= $webTitle ?></title>
+
+    <!-- ======= Favicon ======= -->
+    <link rel="icon" href="img/favicon.png" type="image/x-icon">
+
+    <!-- ======= Librerias o Frameworks ======= -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
