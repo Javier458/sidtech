618518e27155f432bed11db897417cffad02be58
Javier458 <e.javiergalvis@gmail.com>
2024-06-07 17:25:30 -0500

    init page login

---
+++ view/login.php
@@ -0,0 +1,15 @@
+<?php 
+$webTitle = "Inicio | Sid&Tech";
+include_once 'templates/header-html.inc.php';
+?>
+
+<!-- Aquí se agregan los estilos personalizados -->
+<link rel="stylesheet" href="css/index.css">
+
+</head>
+  <body>
+<?php include_once 'templates/nologin-navbar.inc.php'; ?>
+
+
+
+<?php include_once 'templates/end-html.inc.php'; ?>
